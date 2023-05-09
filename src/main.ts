e4cddd4382e3530812ea5cb6b4b627a7cdc04a56
tears <596231290@qq.com>
2023-05-09 14:10:56 +0800

    fix: 删除内网仓库包

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
-const hello = require("node_c");
-console.log(hello);
 app.use(createPinia());
 app.use(router);
 
