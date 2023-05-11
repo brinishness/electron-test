6a23d9433c56e74274406f594273a2a98f66279f
tears <596231290@qq.com>
2023-05-11 14:33:44 +0800

    feat: 测试amd64架构网易云信问题

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -5,26 +5,34 @@
 </template>
 <script lang="ts" setup>
 import {useRoute} from "vue-router";
-import NERtcEngine from "nertc-electron-sdk";
+// import NERtcEngine from "nertc-electron-sdk";
 const NERtcSDK = require("nertc-electron-sdk").default;
-import WebRoomkit from "neroom-web-sdk";
+// import WebRoomkit from "neroom-web-sdk";
+import {ref} from "vue";
+import type {NERtcEngineContext} from "nertc-electron-sdk/types/api/defs";
 
-var nertcEngine = new NERtcSDK.NERtcEngine();
-let ret = nertcEngine.initialize({
-    app_key: "0799fc1781779cc0e754a549d615f95b",
-    log_dir: "./log",
-    log_level: 4,
-    log_file_max_size_KB: 1024,
-    log_file_max_keep_days: 3,
-});
+const isInit = ref<Boolean>(false);
+let nertcEngine = new NERtcSDK.NERtcEngine();
+const initSDK = () => {
+    let context = <NERtcEngineContext>{}
+    context.app_key = '0799fc1781779cc0e754a549d615f95b'
+    context.log_level = 3
+    context.log_dir_path = './log'
+    context.log_file_max_size_KBytes = 0
+    let ret = nertcEngine.initialize(context);
+    isInit.value = true;
+    console.log(`initialize::ret = ${ret}`);
+}
 
-console.log(`initialize::ret = ${ret}`);
+if (!isInit.value) {
+    initSDK();
+}
 
 nertcEngine.on("onJoinChannel", (res: any) => {
     console.log("onJoinChannel", res);
 });
 console.log(nertcEngine);
-console.log(WebRoomkit);
+// console.log(WebRoomkit);
 const route = useRoute();
 console.log(route.query);
 
