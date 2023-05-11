4ca38575a2957d15dbff164a4c284e39355c4fa8
tears <596231290@qq.com>
2023-05-11 09:37:30 +0800

    feat: 测试amd64架构网易云信问题

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="about">
-      <div id="canvas"></div>
-  </div>
+    <div class="about">
+        <div id="canvas"></div>
+    </div>
 </template>
 <script lang="ts" setup>
 import {useRoute} from "vue-router";
 // import NERtcEngine from "nertc-electron-sdk";
 const NERtcEngine = require("nertc-electron-sdk").default;
+NERtcEngine.initialize({
+    app_key: "0799fc1781779cc0e754a549d615f95b",
+    log_dir: "./log",
+    log_level: 4,
+    log_file_max_size_KB: 1024,
+    log_file_max_keep_days: 3,
+});
+NERtcEngine.on("onJoinChannel", (res: any) => {
+    console.log("onJoinChannel", res);
+});
 import WebRoomkit from 'neroom-web-sdk';
 
 console.log(NERtcEngine);
@@ -17,10 +27,10 @@ console.log(route.query);
 </script>
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
 }
 </style>
